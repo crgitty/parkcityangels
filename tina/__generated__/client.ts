6e654aec53682868186da3310510dc9407051f32
colmanreiss <colman.reiss@gmail.com>
2026-06-12 16:23:12 -0600

    Add TinaCMS admin assets and generated files

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Volumes/COLMANREISS/PCA/PCA Web Dev/PCA Git Repo/parkcityangels/tina/__generated__/.cache/1781297997485', url: 'http://localhost:4001/graphql', token: '86abd383facb8930faaf45eafbc3d80370180fd4', queries,  });
+export const client = createClient({ cacheDir: '/Volumes/COLMANREISS/PCA/PCA Web Dev/PCA Git Repo/parkcityangels/tina/__generated__/.cache/1781302863621', url: 'https://content.tinajs.io/1.6/content/073624bf-e0b1-4d9c-9f64-faeb30b7f996/github/main', token: '86abd383facb8930faaf45eafbc3d80370180fd4', queries,  });
 export default client;
   
--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -765,7 +765,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "http://localhost:4001/graphql",
+        url: "https://content.tinajs.io/1.6/content/073624bf-e0b1-4d9c-9f64-faeb30b7f996/github/main",
         queries,
       })
     )
