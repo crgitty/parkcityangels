04e6f09f3a8a48905e92be4eb6ecce8db043a04d
colmanreiss <colman.reiss@gmail.com>
2026-06-12 15:53:37 -0600

    Commit tina generated files, simplify build script

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: '/Volumes/COLMANREISS/PCA/PCA Web Dev/PCA Git Repo/parkcityangels/tina/__generated__/.cache/1781297997485', url: 'http://localhost:4001/graphql', token: '86abd383facb8930faaf45eafbc3d80370180fd4', queries,  });
+export default client;
+  
